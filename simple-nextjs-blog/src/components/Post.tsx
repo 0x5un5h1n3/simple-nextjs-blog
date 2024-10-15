2440ca52f39a63e8d2977967a0e1b69b7448f3e1
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2024-10-15 17:28:17 +0530

    Add delete functionality to Post component

--- simple-nextjs-blog/src/components/Post.tsx
+++ simple-nextjs-blog/src/components/Post.tsx
@@ -1,25 +1,38 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
+import postStore from "../store/PostStore";
 
-const Post = ({ post }) => (
-  <motion.div
-    className="glass"
-    initial={{ scale: 0 }}
-    animate={{ scale: 1 }}
-    transition={{ duration: 0.3 }}
-  >
-    <h2 className="text-xl font-semibold">{post.title}</h2>
-    <p>{post.excerpt}</p>
-    <Link href={`/post/${post.id}`} className="text-blue-500 hover:underline">
-      Read More
-    </Link>
-    <Link
-      href={`/post/edit/${post.id}`}
-      className="text-blue-500 hover:underline ml-4"
+const Post = ({ post }) => {
+  const handleDelete = () => {
+    postStore.posts = postStore.posts.filter((p) => p.id !== post.id);
+  };
+
+  return (
+    <motion.div
+      className="glass"
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{ duration: 0.3 }}
     >
-      Edit
-    </Link>
-  </motion.div>
-);
+      <h2 className="text-xl font-semibold">{post.title}</h2>
+      <p>{post.excerpt}</p>
+      <Link href={`/post/${post.id}`} className="text-blue-500 hover:underline">
+        Read More
+      </Link>
+      <Link
+        href={`/post/edit?id=${post.id}`}
+        className="text-green-500 hover:underline ml-4"
+      >
+        Edit
+      </Link>
+      <button
+        onClick={handleDelete}
+        className="text-red-500 hover:underline ml-4"
+      >
+        Delete
+      </button>
+    </motion.div>
+  );
+};
 
 export default Post;
